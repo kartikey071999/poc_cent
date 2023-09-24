85d8037020d6ee37de5606768cba21b29084760d
Kartikey gupta <kartikey071999@gmail.com>
2023-09-25 04:01:12 +0530

    Update __init__.py
    updation of code according to email content

--- appointement-connection/__init__.py
+++ appointement-connection/__init__.py
@@ -19,16 +19,26 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         if not email or not patient_id or not appointment_date or not appointment_time:
             return func.HttpResponse("Missing required data.", status_code=400)
 
+        email_subject = "Appointment Confirmation"
+        email_body = f"Hello,\n\nYour appointment has been confirmed. Here are the details:\n\n" \
+                     f"Patient ID: {patient_id}\n" \
+                     f"Appointment Date: {appointment_date}\n" \
+                     f"Appointment Time: {appointment_time}\n\n" \
+                     f"Thank you for choosing our service!\n\nBest regards!"
+        
+        
+        
     connection_string = "endpoint=https://con.unitedstates.communication.azure.com/;accesskey=e7HF+eTIl/UsHGjlw0OXB9slfNB8cYEFXXkwxWzk6axkhiAdbFR9ejRC5EqZ1Kpjmc7r47uPcTT4Gz2aZth0wQ=="
     client = EmailClient.from_connection_string(connection_string)
 
     message = {"senderAddress": "DoNotReply@623bac90-fff5-4997-8774-1adc57d02be8.azurecomm.net",
                "recipients": {
-                   "to": [{"address": "kartikey071999@gmail.com"}],
+                   "to": [{"address": email}],
                },
                "content": {
-                   "subject": "Test Email",
-                   "plainText": "Hello world via email.",
+                    "subject": email_subject,
+                    "plainText": email_body,
+                    "html": f"<html><p>{email_body}</p></html>"
                }
                }
 
